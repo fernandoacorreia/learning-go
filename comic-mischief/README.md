6b190b7778dd20a39045a7746f7b86a53c1701f8
Fernando Correia <fernandoacorreia@gmail.com>
2020-09-10 20:58:00 -0700

    Comic Mischief

---
+++ comic-mischief/README.md
@@ -0,0 +1,14 @@
+# Comic Mischief
+
+This program demonstrates the use of variables and types in Go programming to maintain a catalog of items for a comic
+book store.
+
+## Source
+
+Codecademy's [Learn Go](https://www.codecademy.com/learn/learn-go) course.
+
+## Running
+
+```
+bin/go run comic-mischief/main.go
+```
---
+++ comic-mischief/main.go
@@ -0,0 +1,115 @@
+package main
+
+import(
+  "fmt"
+)
+
+func main() {
+  // Comic book variables
+  var publisher, writer, artist, title, genre string
+  var year, pageNumber, age int
+  var grade, price float32
+
+  // Define first comic book
+  title = "Mr. GoToSleep"
+  genre = "Mistery"
+  writer = "Tracey Hatchet"
+  artist = "Jewel Tampson"
+  publisher = "DizzyBooks Publishing Inc."
+  year = 1997
+  pageNumber = 14
+  grade = 6.5
+  age = 2020 - year
+  price = float32(age * pageNumber) * grade / 100.0
+
+  // Print out variables
+  fmt.Println(
+    title,
+    "genre",
+    genre,
+    "written by",
+    writer,
+    "drawn by",
+    artist,
+    "published by",
+    publisher,
+    "on",
+    year,
+    "containing",
+    pageNumber,
+    "pages",
+    "in condition",
+    grade,
+    "price",
+    price,
+  )
+
+  // Define second comic book
+  title = "Epic Vol. 1"
+  genre = "SciFi"
+  writer = "Ryan N. Shawn"
+  artist = "Phoebe Paperclips"
+  publisher = "DizzyBooks Publishing Inc."
+  year = 2013
+  pageNumber = 160
+  grade = 9.0
+  age = 2020 - year
+  price = float32(age * pageNumber) * grade / 100.0
+
+  // Print out variables
+  fmt.Println(
+    title,
+    "genre",
+    genre,
+    "written by",
+    writer,
+    "drawn by",
+    artist,
+    "published by",
+    publisher,
+    "on",
+    year,
+    "containing",
+    pageNumber,
+    "pages",
+    "in condition",
+    grade,
+    "price",
+    price,
+  )
+
+  // Define third comic book
+  title = "Ms. Y"
+  genre = "Adventure"
+  writer = "Gordon Ryan"
+  artist = "Isobelle Leclair"
+  publisher = "Astra Books"
+  year = 2001
+  pageNumber = 47
+  grade = 8.5
+  age = 2020 - year
+  price = float32(age * pageNumber) * grade / 100.0
+
+  // Print out variables
+  fmt.Println(
+    title,
+    "genre",
+    genre,
+    "written by",
+    writer,
+    "drawn by",
+    artist,
+    "published by",
+    publisher,
+    "on",
+    year,
+    "containing",
+    pageNumber,
+    "pages",
+    "in condition",
+    grade,
+    "price",
+    price,
+  )
+
+}
