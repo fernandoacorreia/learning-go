4d39c1aa37e4ba571a59cdeb8fbb4410f9f06302
Fernando Correia <fernandoacorreia@gmail.com>
2020-09-19 09:33:24 -0700

    Edit scan

--- scan/main.go
+++ scan/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-  fmt.Print("What is your name? ")
+  fmt.Print("What is your first name? ")
   var response string
   fmt.Scan(&response)
   fmt.Printf("Hello, %v!\n", response)
