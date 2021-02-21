8ebcb262bb43e818f7eedd3451da01b6e06807a9
Fernando Correia <fernandoacorreia@gmail.com>
2021-02-20 16:02:16 -0800

    Fix formatting

--- random/strings.go
+++ random/strings.go
@@ -29,10 +29,10 @@ func randomString(length int) string {
 
 // Based on John Leidegren's code at https://stackoverflow.com/a/54491783/376366
 func seedRandomizer() {
-    var b [8]byte
-    _, err := crypto_rand.Read(b[:])
-    if err != nil {
-        panic("cannot seed math/rand package with cryptographically secure random number generator")
-    }
-    math_rand.Seed(int64(binary.LittleEndian.Uint64(b[:])))
+  var b [8]byte
+  _, err := crypto_rand.Read(b[:])
+  if err != nil {
+    panic("cannot seed math/rand package with cryptographically secure random number generator")
+  }
+  math_rand.Seed(int64(binary.LittleEndian.Uint64(b[:])))
 }
